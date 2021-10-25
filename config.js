9ad2ad0de95bb528e8d17c1123ef9ca34279b9c9
Cuongjr02 <93130150+Cuongjr02@users.noreply.github.com>
2021-10-25 18:16:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "LOVE",
+    introTitle: 'Đăng Trọng',
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao Trọng cứ đi nhầm đường
+    Lạc vào tim Toàn lẻ loi,
+    Đằng sau chữ yêu đây là thương <3`,
+    btnIntro: '^^ HiHi ^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ?',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Trọng lại yêu Toàn đến thế <3',
+    btnReply: 'Gửi cho Toàn <3',
+    reply: 'Because you are number one in my heart ❤ ',
+    mess: 'Toàn biết mà 🥰. Yêu Trọng nhiều nhiều 😘😘',
+    messDesc: 'Thank you, I love you forever !',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hoangdieu.22022002'
+}   
